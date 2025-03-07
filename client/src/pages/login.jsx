34203061cd9fca280386c169be7d8812fd59c264
MAYUKH GHOSH <ghoshmayukh.janai@gmail.com>
2025-03-07 20:29:33 +0530

    const [user, setUser] = useState({     username: "",     email: "",     phone: "",     password: "",   });
      //Handling Input values
      const handleInput = (e) => {
        console.log(e);
        let name = e.target.name;
        let value = e.target.value;
    
        setUser({
          ...user,
          [name]: value,
        });
      };
    
      //Handling Submission of values
      const handleSubmit = (e) => {
        e.preventDefault();
        console.log(user);
      };
    
      return (
        <>
          <section>
            <main>
              <div className="section-registration">
                <div className="reg-left">
                  <div className="reg-image">
                    <img
                      src="/images/register.png"
                      alt="404"
                      width={500}
                      height={500}
                    />
                  </div>
                </div>
    
                <div className="reg-right">
                  <div className="reg-form">
                    <h1 className="main-heading">Registration Form</h1>
                    <br />
                    <form onSubmit={handleSubmit}>
                      <div>
                        <label htmlFor="username"> Username </label>
                        <input
                          type="text"
                          name="username"
                          placeholder="Enter Name"
                          id="username"
                          required
                          value={user.username}
                          onChange={handleInput}
                        />
                      </div>
                      <div>
                        <label htmlFor="email"> Email </label>
                        <input
                          type="email"
                          name="email"
                          placeholder="Enter Email "
                          id="email"
                          required
                          value={user.email}
                          onChange={handleInput}
                        />
                      </div>
                      <div>
                        <label htmlFor="phone"> Phone </label>
                        <input
                          type="number"
                          name="phone"
                          placeholder="Enter Phone"
                          id="phone"
                          required
                          value={user.phone}
                          onChange={handleInput}
                        />
                      </div>
                      <div>
                        <label htmlFor="password"> Password </label>
                        <input
                          type="text"
                          name="password"
                          placeholder="Enter Password"
                          id="password"
                          required
                          value={user.password}
                          onChange={handleInput}
                        />
                      </div>
                      <br />
                      <button type="sumbit" className="reg-btn">
                        Sign Up
                      </button>
                    </form>
                  </div>
                </div>
              </div>
            </main>
          </section>
        </>
      );
    };

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,3 +1,84 @@
+import { useState } from "react";
+
 export const Login = () => {
-  return <h1>Hello Login Page</h1>;
+  const [login, userLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  //Handling Input values
+  const handleInput = (e) => {
+    console.log(e);
+    let name = e.target.name;
+    let value = e.target.value;
+
+    userLogin({
+      ...login,
+      [name]: value,
+    });
+  };
+
+  //Handling Submission of values
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(login);
+  };
+
+  return (
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="reg-left">
+              <div className="reg-image">
+                <img
+                  src="/images/login.png"
+                  alt="404"
+                  width={500}
+                  height={500}
+                />
+              </div>
+            </div>
+
+            <div className="reg-right">
+              <div className="reg-form">
+                <h1 className="main-heading">Login Form</h1>
+                <br />
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="email"> Email </label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Enter Email "
+                      id="email"
+                      required
+                      value={login.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password"> Password </label>
+                    <input
+                      type="text"
+                      name="password"
+                      placeholder="Enter Password"
+                      id="password"
+                      required
+                      value={login.password}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <br />
+                  <button type="sumbit" className="reg-btn">
+                    Login
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
 };
